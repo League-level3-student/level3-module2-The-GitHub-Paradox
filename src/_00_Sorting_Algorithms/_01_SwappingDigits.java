6dea8ff470636160dcc804f7b95881ce2bc29b7e
Meme1sh_ <60296686+The-GitHub-Paradox@users.noreply.github.com>
2021-01-16 15:00:26 -0800

    e
    e

--- src/_00_Sorting_Algorithms/_01_SwappingDigits.java
+++ src/_00_Sorting_Algorithms/_01_SwappingDigits.java
@@ -24,14 +24,14 @@ class _01_SwappingDigits {
 	
 	//1. Complete the method so that the first two elements of the array are swapped
 	public static void swapArrayOfTwo(int[] arr) {
-		
+	
 	}
 	
 	//2. Complete the method so that it sorts the array using a bubble sort.
 	//   Iterate through the array and when you find two elements that are out
 	//   of order, swap them. Repeat this until the array is in order.
 	public static void sortIntArray(int[] arr) {
-		
+	
 	}
 	
 	//3. Complete the method so that it finds the middle number in the array.
@@ -39,5 +39,6 @@ class _01_SwappingDigits {
 	//   *Double Hint* Use the method you already wrote in step 2 to sort it
 	public static int findMiddle(int[] arr) {
 		return 0;
+	
 	}
 }
